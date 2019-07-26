1d1f3cf46ed1ab3596c361a0aac20b5b4f6ec19a
tiffanychu90 <tiffany.chu@lacity.org>
2019-07-26 16:13:10 +0000

    Add utils

---
+++ utils.py
@@ -0,0 +1,5 @@
+# utils contains the variables or functions that would be used across several notebooks
+# These are saved and updated here. The notebooks could then be rerun without too many manual changes.
+
+# Convert square foot into acres
+feet_in_acres = 43560
